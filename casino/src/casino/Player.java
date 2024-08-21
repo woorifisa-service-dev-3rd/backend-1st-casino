b6cdcf82f1cf385bcab9c7ee8ce1cd7141a36bd2
bangsk <cats7858@gmail.com>
2024-08-21 17:29:33 +0900

    docs :  menu 항목 함수로 분리

--- casino/src/casino/Player.java
+++ casino/src/casino/Player.java
@@ -13,13 +13,12 @@ public class Player {
 
     public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
-        Scanner input = new Scanner(System.in);
         Connection connection = DatabaseUtil.getConnection();
 
         System.out.println("Welcome to Casino!");
 
         System.out.print("이름을 입력하세요: ");
-        String name = input.nextLine();
+        String name = sc.nextLine();
 
 //        System.out.println("connection = " + connection);
         long playerId = createAccount(connection, name);
@@ -32,15 +31,102 @@ public class Player {
 
     // 메뉴 선택 함수
     private static void handleMenu(Connection connection, Scanner sc, String name) throws SQLException {
-    String playerIdQuery = "SELECT id FROM player WHERE name = ?";
-    PreparedStatement playerIdStatement = connection.prepareStatement(playerIdQuery);
-    playerIdStatement.setString(1, name);
-    ResultSet playerIdResultSet = playerIdStatement.executeQuery();
+        long playerId = getPlayerIdByName(connection, name);
+        int balance = getPlayerBalance(connection, playerId);
 
-    if (playerIdResultSet.next()) {
-        long playerId = playerIdResultSet.getLong("id");
+        while (true) {
+            System.out.println();
+            System.out.println(name + "님의 현재 잔액: " + balance);
+            if (balance < 0) {
+                System.out.println("잔액이 부족하여 강제로 종료합니다.");
+                System.exit(0);
+            }
+            System.out.println("==== MENU ====");
+            System.out.println("1. 게임시작");
+            System.out.println("2. 대출하기");
+            System.out.println("3. 오늘의 게임왕 보기");
+            System.out.println("4. 종료");
 
-        // play_wallet에서 잔액 가져오기
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    startGame(name);
+                    break;
+                case 2:
+                    handleLoan(name, connection, playerId, balance);
+                    balance = getPlayerBalance(connection, playerId);
+                    break;
+                case 3:
+                    todayKing();
+                    break;
+                case 4:
+                    exitCasino(sc);
+                    break;
+
+                default:
+                    System.out.println("잘못된 선택입니다. 다시 시도하세요.");
+                    break;
+            }
+        }
+    }
+
+    // ----------M E N U---------
+    // 1. 게임시작
+    private static void startGame(String name) {
+        System.out.println(name + "님 게임을 시작합니다.");
+        // 게임 시작 로직
+    }
+
+    // 2. 대출하기
+    private static void handleLoan(String name, Connection connection, long playerId, int balance) throws SQLException {
+        System.out.println(name + "님 은행으로 이동합니다.");
+        // 대출 후 잔액 계산 (예시로 Bank 클래스의 bankLoan 메서드 사용)
+        long newBalance = Bank.bankLoan(balance);
+        System.out.println("대출 후 잔액: " + newBalance);
+
+        // 남은 판 수 계산 (예시로 Bank 클래스의 leftGameCnt 메서드 사용)
+        int remainingGames = Bank.leftGameCnt();
+        System.out.println("남은 판 수: " + remainingGames);
+        updatePlayerBalance(connection, playerId, newBalance);
+        balance = (int) newBalance;
+    }
+
+    // 3. 오늘의게임황
+    private static void todayKing() {
+        System.out.println("오늘의 게임왕입니다!");
+        //게임왕 코드
+    }
+
+    // 4. 종료
+    private static boolean exitCasino(Scanner sc) throws SQLException {
+        System.out.println("카지노를 떠나시겠습니까? (y/n) : ");
+        String exitCasino = sc.nextLine();
+        if (exitCasino.equalsIgnoreCase("y")) {
+            System.out.println("다음에 또 방문해주세요");
+            return true;
+        }
+        return false;
+    }
+
+    //player name으로 id 가져오는 함수
+    private static long getPlayerIdByName(Connection connection, String name) throws SQLException {
+        String playerIdQuery = "SELECT id FROM player WHERE name = ?";
+        PreparedStatement playerIdStatement = connection.prepareStatement(playerIdQuery);
+        playerIdStatement.setString(1, name);
+        ResultSet playerIdResultSet = playerIdStatement.executeQuery();
+
+        if (playerIdResultSet.next()) {
+            long playerId = playerIdResultSet.getLong("id");
+            playerIdStatement.close();
+            return playerId;
+        }
+        playerIdStatement.close();
+        throw new SQLException();
+    }
+
+    // play_wallet에서 잔액 가져오는 함수
+    private static int getPlayerBalance(Connection connection, long playerId) throws SQLException {
         String balanceQuery = "SELECT balance FROM play_wallet WHERE player_id = ?";
         PreparedStatement balanceStatement = connection.prepareStatement(balanceQuery);
         balanceStatement.setLong(1, playerId);
@@ -48,67 +134,23 @@ public class Player {
 
         if (balanceResultSet.next()) {
             int balance = balanceResultSet.getInt("balance");
-
-            while (true) {
-                System.out.println();
-                System.out.println(name + "님의 현재 잔액: " + balance);
-                if (balance < 0) {
-                    System.out.println("잔액이 부족하여 강제로 종료합니다.");
-                    System.exit(0);
-                }
-                System.out.println("==== MENU ====");
-                System.out.println("1. 게임시작");
-                System.out.println("2. 대출하기");
-                System.out.println("3. 오늘의 게임왕 보기");
-                System.out.println("4. 종료");
-
-                int choice = sc.nextInt();
-                sc.nextLine();
-                switch (choice) {
-                    case 1:
-                        System.out.println(name + "님 게임을 시작합니다.");
-                        //게임 시작 코드
-                        break;
-                    case 2:
-                        System.out.println(name + "님 은행으로 이동합니다.");
-                        // 대출 후 잔액 계산 (예시로 Bank 클래스의 bankLoan 메서드 사용)
-                        long newBalance = Bank.bankLoan(balance);
-                        System.out.println("대출 후 잔액: " + newBalance);
-
-                        // 남은 판 수 계산 (예시로 Bank 클래스의 leftGameCnt 메서드 사용)
-                        int remainingGames = Bank.leftGameCnt();
-                        System.out.println("남은 판 수: " + remainingGames);
-
-                        // 업데이트된 잔액을 play_wallet 테이블에 저장
-                        String updateQuery = "UPDATE play_wallet SET balance = ? WHERE player_id = ?";
-                        PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
-                        updateStatement.setLong(1, newBalance);
-                        updateStatement.setLong(2, playerId);
-                        updateStatement.executeUpdate();
-
-                        updateStatement.close();
-                        balance = (int) newBalance;
-                        break;
-                    case 3:
-                        System.out.println("오늘의 게임왕입니다!");
-                        //게임왕 코드
-                        break;
-                    case 4:
-                        System.out.println("카지노를 떠나시겠습니까? (y/n) : ");
-                        String exitCasino = sc.nextLine();
-                        if (exitCasino.equalsIgnoreCase("y")) {
-                            System.out.println("다음에 또 방문해주세요");
-                            return;
-                        }
-                        break;
-                    default:
-                        System.out.println("잘못된 선택입니다. 다시 시도하세요.");
-                        break;
-                }
-            }
+            balanceStatement.close();
+            return balance;
         }
+        balanceStatement.close();
+        throw new SQLException();
     }
-}
+
+    // 업데이트된 잔액을 play_wallet 테이블에 저장하는 함수
+    private static void updatePlayerBalance(Connection connection, long playerId, long newBalance) throws SQLException {
+        String updateQuery = "UPDATE play_wallet SET balance = ? WHERE player_id = ?";
+        PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+        updateStatement.setLong(1, newBalance);
+        updateStatement.setLong(2, playerId);
+        updateStatement.executeUpdate();
+        updateStatement.close();
+    }
+
     // 계정 생성 함수
     private static long createAccount(Connection connection, String name) throws SQLException {
         String insertPlayerQuery = "INSERT INTO player (name) VALUES (?)";
@@ -128,6 +170,7 @@ public class Player {
         insertPlayerStatement.close();
         throw new SQLException("계정 생성에 실패했습니다.");
     }
+
     //월렛 생성 함수
     private static void createWallet(Connection connection, long playerId) throws SQLException {
         // 초기 금액을 10만원으로 설정하여 play_wallet에 데이터 삽입
@@ -152,3 +195,5 @@ public class Player {
 
 
 }
+
+
