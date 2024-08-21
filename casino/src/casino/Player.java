5dd00e3e4117fe7b6d60b5318663a64f0eecb6b7
3-34\3-34 <cbj0010@naver.com>
2024-08-21 12:09:46 +0900

    chore : db연동 확인

--- casino/src/casino/Player.java
+++ casino/src/casino/Player.java
@@ -1,20 +1,23 @@
 package casino;
 
+import db.DatabaseUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Player {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("여기서 어쩌고 run하면 됩니다.");
-		/*
-		 * Console.read() // 문자열 입력받는 메소드 Console.readInt() // 정수 입력받는 메소드
-		 * Console.readDouble() // 실수 입력받는 메소드
-		 * 
-		 * 출력할때
-		 * 
-		 * Console.write() // 문자열/정수/실수 출력하는 메소드 Console.writeln() // 출력 후 줄바꿈도 하고 싶을때
-		 * 쓰는 메소드
-		 */
-		
-	}
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DatabaseUtil.getConnection();
+        System.out.println("connection = " + connection);
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from player");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        System.out.println(resultSet.getString("name"));
 
+        System.out.println("여기서 어쩌고 run하면 됩니다.");
+
+    }
 }
