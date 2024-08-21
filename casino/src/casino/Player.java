424e7066df9d02478b061717967fe80e20801944
Hyunah Lee <hyuna030619@naver.com>
2024-08-21 14:33:04 +0900

    feature: player update

--- casino/src/casino/Player.java
+++ casino/src/casino/Player.java
@@ -121,18 +121,3 @@ public class Player {
 
 
 
-
-
-/*
- * Console.read() // 문자열 입력받는 메소드 Console.readInt() // 정수 입력받는 메소드
- * Console.readDouble() // 실수 입력받는 메소드
- *
- * 출력할때
- *
- * Console.write() // 문자열/정수/실수 출력하는 메소드 Console.writeln() // 출력 후 줄바꿈도 하고 싶을때
- * 쓰는 메소드
- */
-
-
-
-
