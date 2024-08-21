b3ec5a882c99c977f71da471b38b834600185b15
3-34\3-34 <cbj0010@naver.com>
2024-08-21 15:11:15 +0900

    feat : select wallet DAO생성

--- casino/src/model/Wallet.java
+++ casino/src/model/Wallet.java
@@ -2,9 +2,18 @@ package model;
 
 public class Wallet {
     int id;             //walletId
+    int playerId;
     int balance;        //잔액
     boolean loan;       //  대출여부
-    int loanAmount;     //대출 잔액(대출 시)
+    int loanAmount = 0;     //대출 잔액(대출 시)
     int remainingGame;  //남은 판수(대출시)
 
+    public Wallet(int id, int playerId, int balance, boolean loan, int loanAmount, int remainingGame) {
+        this.id = id;
+        this.playerId = playerId;
+        this.balance = balance;
+        this.loan = loan;
+        this.loanAmount = loanAmount;
+        this.remainingGame = remainingGame;
+    }
 }
